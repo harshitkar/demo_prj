6229f213b687ec7bf22ee2eba04a707bba5228e7
harshitkar <harshitkar104@gmail.com>
2024-03-30 16:02:47 +0530

    Create and Join group Backend Completed

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -12,13 +12,6 @@ public class Main extends JFrame {
 //        loginFrame.setSize(384, 305);
 //        loginFrame.setResizable(false);
 //        loginFrame.setVisible(true);
-//        Group user1 = new Group("1", "Harsh");
-//        Group user2 = new Group("2", "Kunal");
-//        Group user3 = new Group("3", "Arya");
-//        DefaultListModel<Group> model = new DefaultListModel<>();
-//        model.addElement(user1);
-//        model.addElement(user2);
-//        model.addElement(user3);
         String currentUser = new DataBaseHelper().getCurrentUsername("user2@gmail.com");
         GroupListPage groupsPage = new GroupListPage(currentUser);
         groupsPage.setSize(935, 585);
